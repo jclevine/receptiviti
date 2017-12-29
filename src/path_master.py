5a51459b7d8bed98e590e5f40cd1715fc73d45f5
Justin Levine <jclevine.receipts@gmail.com>
2017-12-29 13:45:08 -0500

    now returns -1 if no edges exist and you try to calculate any path

--- src/path_master.py
+++ src/path_master.py
@@ -6,14 +6,12 @@ class PathMaster:
     def is_not_asking_for_distance_of_anything(self, path):
         return not self._edges and not path
 
-    # TODO: jlevine - Could make it more "clever" possibly
-    def validate_edges_and_vertices(self, path):
-        if self._edges and not path:
-            raise TypeError('No path specified: {}'.format(path))
-        elif not self._edges and path:
-            raise TypeError('No edges specified in path master: {}'.format(self._edges))
+    def no_possible_path_with_no_edges(self, path):
+        return not self._edges and path
 
     def calculate_distance(self, path):
         if self.is_not_asking_for_distance_of_anything(path):
             return 0.0
-        self.validate_edges_and_vertices(path)
+
+        if self.no_possible_path_with_no_edges(path):
+            return -1
--- test/test_path_master.py
+++ test/test_path_master.py
@@ -35,7 +35,7 @@ class TestPathMaster(TestCase):
         actual = path_master.calculate_distance(None)
         self.assertEqual(0.0, actual)
 
-    # TODO: jlevine - Create own error if it seems necessary
-    def test_raises_type_error_if_no_edges_and_any_vertices(self):
+    def test_returns_neg_1_if_no_edges_and_any_vertices(self):
         path_master = PathMaster(None)
-        self.assertRaisesRegex(TypeError, 'No edges specified.*', path_master.calculate_distance, ['AB3'])
+        actual = path_master.calculate_distance(['ABC'])
+        self.assertEqual(-1, actual)
