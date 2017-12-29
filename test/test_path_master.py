dede186bd69dfb694ab38b1eaafcc2cef6f45f1e
Justin Levine <jclevine.receipts@gmail.com>
2017-12-29 13:15:32 -0500

    path master doesn't fail when initializing with no edges

--- test/test_path_master.py
+++ test/test_path_master.py
@@ -6,7 +6,12 @@ from src.path_master import PathMaster
 class TestPathMaster(TestCase):
     edges = ['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']
 
+    def test_initialize_graph_with_no_edges(self):
+        self.assertIsNotNone(PathMaster(None))
+
+    def test_initialize_graph_with_edges(self):
+        self.assertIsNotNone(PathMaster(self.edges))
+
     def test_initialize_graph(self):
         self.assertIsNotNone(PathMaster(self.edges))
 
-    # def
