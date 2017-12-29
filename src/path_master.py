07863f92371dd45036863362fa0ae71b99aba801
Justin Levine <jclevine.receipts@gmail.com>
2017-12-29 13:21:07 -0500

    path master calculates 0 distance with no edges and not vertices specified

--- src/path_master.py
+++ src/path_master.py
@@ -2,3 +2,6 @@ class PathMaster:
 
     def __init__(self, edges):
         pass
+
+    def calculate_distance(self, path):
+        return 0.0
--- test/test_path_master.py
+++ test/test_path_master.py
@@ -15,3 +15,7 @@ class TestPathMaster(TestCase):
     def test_initialize_graph(self):
         self.assertIsNotNone(PathMaster(self.edges))
 
+    def test_calculates_0_distance_with_no_edges_and_no_vertices_specified(self):
+        path_master = PathMaster(None)
+        actual = path_master.calculate_distance(None)
+        self.assertEqual(0.0, actual)
