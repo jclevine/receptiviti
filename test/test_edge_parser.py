e4d64af03ebf766028c7488eeaedc48d4a516351
Justin Levine <jclevine.receipts@gmail.com>
2017-12-29 13:00:59 -0500

    parse edge list returns empty list with empty list

--- test/test_edge_parser.py
+++ test/test_edge_parser.py
@@ -4,6 +4,10 @@ from src.edge_parser import parse_edge_list
 
 
 class TestEdgeParser(TestCase):
-    def test_return_empty_list_with_no_list(self):
+    def test_returns_empty_list_with_no_list(self):
         actual = parse_edge_list(None)
+        self.assertEqual([], actual)
+
+    def test_returns_empty_list_with_empty_list(self):
+        actual = parse_edge_list([])
         self.assertEqual([], actual)
