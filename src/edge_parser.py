0ee55a8c09a157e55c32b1c16163bfe1167420bf
Justin Levine <jclevine.receipts@gmail.com>
2017-12-29 13:09:37 -0500

    parse edge list returns edge list with one edge

--- src/edge_parser.py
+++ src/edge_parser.py
@@ -1,2 +1,4 @@
 def parse_edge_list(edges):
-    return []
+    if edges is None:
+        return []
+    return [' '.join([*edge]) for edge in edges]
--- test/test_edge_parser.py
+++ test/test_edge_parser.py
@@ -11,3 +11,7 @@ class TestEdgeParser(TestCase):
     def test_returns_empty_list_with_empty_list(self):
         actual = parse_edge_list([])
         self.assertEqual([], actual)
+
+    def test_returns_edge_list_with_one_edge(self):
+        actual = parse_edge_list(['AB3'])
+        self.assertEqual(['A B 3'], actual)
