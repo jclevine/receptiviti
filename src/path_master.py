b53d81571968523c62cb101fbd8f44df97022eb5
Justin Levine <jclevine.receipts@gmail.com>
2017-12-31 10:47:54 -0500

    'Privatize' functions and doc one function

--- src/path_master.py
+++ src/path_master.py
@@ -15,40 +15,24 @@ class PathMaster:
                 create_using=nx.DiGraph()
             )
 
-    def is_not_asking_for_distance_of_anything(self, path):
-        return not self._edges and not path
-
-    def no_possible_path_with_no_edges(self, path):
-        return not self._edges and path
-
-    def does_path_exist(self, edge_tuples):
-        return all([self._directed_graph.has_edge(*edge) for edge in edge_tuples])
-
-    @staticmethod
-    def is_path_one_vertex(path):
-        return len(path) == 1
-
-    def any_vertex_does_not_exist(self, path):
-        return bool(set(path) - set(nx.nodes(self._directed_graph)))
-
     def calculate_distance(self, path):
         # Special Cases
-        if self.is_not_asking_for_distance_of_anything(path):
+        if self._is_not_asking_for_distance_of_anything(path):
             return 0.0
 
-        if self.no_possible_path_with_no_edges(path):
+        if self._no_possible_path_with_no_edges(path):
             return -1
 
-        if self.is_path_one_vertex(path):
-            return 0.0 if self.is_path_one_vertex(path) and path[0] in nx.nodes(self._directed_graph) else -1
+        if self._is_path_one_vertex(path):
+            return 0.0 if path[0] in nx.nodes(self._directed_graph) else -1
 
         # Normal Cases
-        edge_tuples = self.build_edge_tuples(path)
+        edge_tuples = self._build_edge_tuples(path)
 
-        if self.any_vertex_does_not_exist(path):
+        if self._any_vertex_does_not_exist(path):
             return -1
 
-        if not self.does_path_exist(edge_tuples):
+        if not self._does_path_exist(edge_tuples):
             return 'NO SUCH ROUTE'
 
         return sum([self._directed_graph.get_edge_data(*edge)['weight'] for edge in edge_tuples])
@@ -83,7 +67,13 @@ class PathMaster:
             return all_paths
 
     @staticmethod
-    def build_edge_tuples(path):
+    def _build_edge_tuples(path):
+        """
+        Convenience function to turn vertex list into edge tuples
+        Eg.
+        Input: ['A', 'B', 'C']
+        Output: [('A', 'B'), ('B', 'C')]
+        """
         return [tuple(path[i:i + 2]) for i in range(len(path) - 1)]
 
     def shortest_path_distance(self, start, end):
@@ -120,3 +110,18 @@ class PathMaster:
                 total += 1 + self.max_dfs(potential_paths, max_distance - path)
         return total
 
+    def _is_not_asking_for_distance_of_anything(self, path):
+        return not self._edges and not path
+
+    def _no_possible_path_with_no_edges(self, path):
+        return not self._edges and path
+
+    def _does_path_exist(self, edge_tuples):
+        return all([self._directed_graph.has_edge(*edge) for edge in edge_tuples])
+
+    @staticmethod
+    def _is_path_one_vertex(path):
+        return len(path) == 1
+
+    def _any_vertex_does_not_exist(self, path):
+        return bool(set(path) - set(nx.nodes(self._directed_graph)))
